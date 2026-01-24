356ce0ecc4a67fa11e49a468bd1b859eb8a70705
Yohann Zaoui <yohannzaoui@gmail.com>
2026-01-24 15:52:05 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,4 @@
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('sw.js');
-}
+self.addEventListener('fetch', (event) => {
+    event.respondWith(fetch(event.request));
+});
+
