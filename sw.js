3e6217ec2b78084563c438d4fbed11218badd9ba
Yohann Zaoui <yohannzaoui@gmail.com>
2026-01-25 13:05:41 +0100

    update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,43 @@
+const CACHE_NAME = 'Technics Master Edition';
+const ASSETS = [
+  './',
+  './index.html',
+  './style.css',
+  './script.js',
+  '.img/favicon.png',
+  '.img/HR_logo.png',
+  '.img/mash_logo_t.png',
+  '.img/favicon.png',
+  '.img/Technics_cover.png',
+  '.img/Technics-Logo-org.png',
+];
+
+// Installation : Mise en cache des fichiers
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Activation : Nettoyage des anciens caches
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Stratégie : Cache First (priorité au cache pour la vitesse)
 self.addEventListener('fetch', (event) => {
-    event.respondWith(fetch(event.request));
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
 
